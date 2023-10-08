dd242419b256969c0949308d4ce8c3c9db8e20f2
Miles Kent <64712073+AlexKCA@users.noreply.github.com>
2023-10-08 16:43:29 -0700

    fixed

--- Lab2/move.hpp
+++ Lab2/move.hpp
@@ -3,32 +3,32 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include "pokemon.hpp"
+//#include "pokemon.hpp"
 using namespace std;
 
 class Move {
-public:
-	// Default constructor
-	Move() : name("Tackle"), type("Normal"), damage(0) {};
-	Move(string nIn, string tIn, int dmg) : name(nIn), type(tIn), damage(dmg){};
+	public:
+		// Default constructor
+		Move() : name("Tackle"), type("Normal"), damage(0) {};
+		Move(string nIn, string tIn, int dmg) : name(nIn), type(tIn), damage(dmg){};
 
-	// Accessors
-	string getName() { return name; };
-	string getType() { return type; };
-	int getDamage() { return damage; };
+		// Accessors
+		string getName() { return name; };
+		string getType() { return type; };
+		int getDamage() { return damage; };
 
-	// Mutators
-	void setName(string nm) { name = nm; };
-	void setType(string ty) { type = ty; };
-	int calculateDamage(int attack, int tDefence);
+		// Mutators
+		void setName(string nm) { name = nm; };
+		void setType(string ty) { type = ty; };
+		int calculateDamage(int attack, int tDefence);
 
 
 
-private:
-	string name;
-	string type;
-	int damage;
-	string targetType;
+	private:
+		string name;
+		string type;
+		int damage;
+		string targetType;
 
 };
 
